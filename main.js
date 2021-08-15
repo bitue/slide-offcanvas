a1f645a8e0b47aee1941c9716fbfeb7424462b29
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-08-16 03:13:06 +0600

    aaa

---
+++ main.js
@@ -0,0 +1,7 @@
+let slide = document.getElementById("slide")
+let menu = document.getElementById("menu")
+
+menu.addEventListener("click", function () {
+    slide.classList.toggle("-translate-x-full")
+    console.log(1)
+})
